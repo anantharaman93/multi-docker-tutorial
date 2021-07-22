5dc2e1083e6f08efa99a9c890eaaac728657618c
Anantharaman G <suraj1291993@gmail.com>
2021-07-22 09:01:20 +0530

    Minor changes

--- client/src/App.js
+++ client/src/App.js
@@ -9,17 +9,17 @@ function App() {
   return (
     <Router>
       <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-Title">Welcome to react</h1>
-        <Link to="/">Home</Link>
-        <Link to="/otherpage">Other Page</Link>
-      </header>
-      <div>
-        <Route exact path="/" component={Fib}/>
-        <Route exact path="/otherpage" component={OtherPage}/>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-Title">Fib calculator V2</h1>
+          <Link to="/">Home</Link>
+          <Link to="/otherpage">Other Page</Link>
+        </header>
+        <div>
+          <Route exact path="/" component={Fib} />
+          <Route exact path="/otherpage" component={OtherPage} />
+        </div>
       </div>
-    </div>
     </Router>
   );
 }
